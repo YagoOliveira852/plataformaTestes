d29cc2cf9c57f812fa277dfe16db3cbc72401bd8
Yago Oliveira <yago.costa852@gmail.com>
2022-11-14 22:12:25 -0300

    Adição de um Pré-texto antes da requisição pro backend

--- frontend/src/components/collapse/collapse.js
+++ frontend/src/components/collapse/collapse.js
@@ -1,28 +1,54 @@
 import { Collapse } from 'antd';
 import React from 'react';
 const { Panel } = Collapse;
-const text = `
-O Resultado aparecerá aqui após a requisição ser executada.
-`;
 
 const CollapseComponent = ({ResBasico, ResAlternativo, ResExcecao, ResGeral}) => {
   const onChange = (key) => {
     console.log(key);
   };
+  
+  var textBasico = `
+  O Resultado do Fluxo Básico aparecerá aqui após a requisição ser executada.
+  `;
+  var textAlternativo = `
+  O Resultado do Fluxo Alternativo aparecerá aqui após a requisição ser executada.
+  `;
+  var textExcecao = `
+  O Resultado do Fluxo de Exceção aparecerá aqui após a requisição ser executada.
+  `;
+  var textGeral = `
+  O Resultado do Fluxo Geral aparecerá aqui após a requisição ser executada.
+  `;
+ 
+  if(JSON.stringify(ResBasico.data, null, 2) !== undefined){
+    textBasico = JSON.stringify(ResBasico.data, null, 2)
+  }
+
+  if(JSON.stringify(ResAlternativo.data, null, 2) !== undefined){
+    textAlternativo = JSON.stringify(ResAlternativo.data, null, 2)
+  }
+
+  if(JSON.stringify(ResExcecao.data, null, 2) !== undefined){
+    textExcecao = JSON.stringify(ResExcecao.data, null, 2)
+  }
+
+  if(JSON.stringify(ResGeral.data, null, 2) !== undefined){
+    textGeral = JSON.stringify(ResGeral.data, null, 2)
+  }
 
   return (
-    <Collapse defaultActiveKey={['']} onChange={onChange}>
+    <Collapse defaultActiveKey={['1']} onChange={onChange}>
       <Panel header="Resultado do Fluxo de Basico" key="1">
-        <pre>{(JSON.stringify(ResBasico.data, null, 2))}</pre>
+        <pre>{textBasico}</pre>
       </Panel>
-      <Panel header="Resultado do Fluxo de Alternativo" key="2">
-        <pre>{JSON.stringify(ResAlternativo.data, null, 2)}</pre>
+      <Panel header="Resultado do Fluxo Alternativo" key="2">
+        <pre>{textAlternativo}</pre>
       </Panel>
-      <Panel header="Resultado do Fluxo de Execeção" key="3">
-        <pre>{JSON.stringify(ResExcecao.data, null, 2)}</pre>
+      <Panel header="Resultado do Fluxo de Exceção" key="3">
+        <pre>{textExcecao}</pre>
       </Panel>
       <Panel header="Resultado do Fluxo Geral" key="4">
-        <pre>{JSON.stringify(ResGeral.data, null, 2)}</pre>
+        <pre>{textGeral}</pre>
       </Panel>
     </Collapse>
   );
